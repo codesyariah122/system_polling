4cd11af6e02ef8519252a8122110329f7b974333
pujiermanto <pujiermanto@gmail.com>
2020-08-13 20:55:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
 
       <title>Polling css framework</title>
     </head>
+    
+    <body>
 
     <div id="polling">
       <?php html($dir, 'polling', '.php', 'Polling Framework css'); ?>
     </div>
 
-
-    <body>
       <!--Import jQuery before materialize.js-->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
